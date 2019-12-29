644ce48bf416bd5be873825d0d75742a8f905139
JSiow <julian.k.siow@gmail.com>
2019-12-29 13:31:54 +0100

    adds form capture

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,8 @@
+// Global Variables
+let players = []
+
+const starters = ['fire', 'water', 'grass']
+
 const starterFinder = (players, starters) => {
   players.forEach(player => {
     num = Math.floor(Math.random() * starters.length);
@@ -8,3 +13,14 @@ const starterFinder = (players, starters) => {
     console.log(match);
   });
 };
+
+$("#game-info").submit((event) => {
+  event.preventDefault();
+  players = [
+    $('#player1Name').val(), 
+    $('#player2Name').val(), 
+    $('#player3Name').val(),
+  ];
+  console.log($("#genSelect").val())
+  starterFinder(players, starters)
+});
